1fb5b3d18e6ac682a701599d8a44dc3615aae1dd
susmitakarmaker <susmitakarmaker69@gmail.com>
2026-05-15 16:18:53 +0600

    print

---
+++ Comments.py
@@ -0,0 +1,12 @@
+# Single line comments :
+# Print your name :
+
+print("Susmita Karmaker")
+
+""" 
+Multiline comments:
+print your Institute name:
+"""
+
+print("Rangpur Enginnering College")
+
---
+++ Text.py
@@ -0,0 +1,7 @@
+print("Hello World!", end=" ")
+
+print("I will print on the same line.")
+
+print("I am", 35, "years old.")
+
+
