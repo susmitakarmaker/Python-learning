2c4d15cdc6a725606f6915b5a908228ba5fbd650
susmitakarmaker <susmitakarmaker69@gmail.com>
2026-05-15 21:53:23 +0600

    check

---
+++ Array.py
@@ -0,0 +1,28 @@
+cars = ["Ford", "Volvo", "BMW"]
+
+x = cars[1]
+
+print(x)
+
+cars = ["Ford", "Volvo", "BMW"]
+
+for y in cars:
+  print(y)
+
+car = ["Ford", "Volvo", "BMW"]
+
+car.pop(1)
+
+print(car)
+
+# Create a list
+c = ["Ford", "Volvo", "BMW"]
+
+# Print the first item
+print(c[0])
+
+# Change the second item to "Toyota"
+c[1] = "Toyota"
+
+# Print the list
+print(c)
