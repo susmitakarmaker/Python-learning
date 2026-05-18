70c0ebc19c385fffd593f8bfeaae92e0c89ebd62
susmitakarmaker <susmitakarmaker69@gmail.com>
2026-05-18 09:13:55 +0600

    check

---
+++ String-Methods.py
@@ -0,0 +1,28 @@
+# UpperCase
+
+s = "Gobeshona Learning"
+print(s.upper())
+
+# LowerCase
+
+s = "GOBESHONA LEARNING"
+print(s.lower())
+
+# Strip() : Leading ending space remove
+
+a = " Gobeshona"
+print(a)
+print(a.strip())
+
+# Replace
+
+s = "Gobeshona Learning"
+print(s)
+print(s.replace('Learning' , 'Academy'))
+
+# Split
+
+x = "Gobeshona Learning"
+print(x.split(","))
+print(len(s))
+print(s.count("n"))
